0e154f3eefd2eba74f2b6fe49eba4244d0a40f70
Seth Gillett <seth.gillett@pm.me>
2021-09-04 01:53:31 -0400

    Added span property to gallery images

--- components/gallery.js
+++ components/gallery.js
@@ -5,9 +5,9 @@ vueComps.gallery = {
     <div class="container-fluid bg-3 text-center">    
       <h3>Previous Commissions</h3><br>
       <div v-for="row in images" class="row">
-        <div v-for="img in row" class="col-sm-6">
+        <div v-for="img in row" :class="'col-sm-' + img.span">
           <div class="thumbnail">
-            <img v-bind:src="img.path" class="img-responsive" style="width:100%" alt="Dog image">
+            <img :src="img.path" class="img-responsive" style="width:100%" alt="Dog image">
             <div class="caption">
               <p>{{ img.caption }}</p>
             </div>
